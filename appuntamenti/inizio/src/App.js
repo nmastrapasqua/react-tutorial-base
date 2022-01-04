dde09f3d62f01a8e850d057d4c9ca57863cf3a1f
Nunzio Mastrapasqua <nunzio.mastrapasqua@libero.it>
2022-01-04 17:45:39 +0100

    Fine progetto 'Appuntamenti'

--- appuntamenti/inizio/src/App.js
+++ appuntamenti/inizio/src/App.js
@@ -1,11 +1,39 @@
 import List from "./List";
 import data from "./data";
+import { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  console.log(people);
+
+  const reloadAllItem = () => {
+    setPeople(data);
+  };
+
+  const removeItem = (id) => {
+    console.log("elimino");
+    setPeople((oldValue) => {
+      return oldValue.filter((value) => value.id !== id);
+    });
+  };
+
   return (
-    <div className="App">
-      <h2> App</h2>
-    </div>
+    <section>
+      <div className="container">
+        <h2 style={{ color: "var(--bg-blue)" }}>Prossimi Incontri</h2>
+        <div className="people-list">
+          <List data={people} removeItem={removeItem} />
+        </div>
+        <div className="btn-group">
+          <button className="btn btn-reset" onClick={reloadAllItem}>
+            Reload
+          </button>
+          <button className="btn btn-delete" onClick={() => setPeople([])}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- appuntamenti/inizio/src/List.js
+++ appuntamenti/inizio/src/List.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { RiDeleteBackFill } from "react-icons/ri";
 
-const List = () => {
-  return <ul></ul>;
+const List = (props) => {
+  console.log(props);
+  return (
+    <ul className="user-list">
+      {props.data.map((person) => (
+        <li key={person.id}>
+          <Person {...person} removeItem={props.removeItem} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
-const Person = () => {
-  return <article>perosna</article>;
+const Person = ({ id, nome, stato, img, removeItem }) => {
+  return (
+    <article>
+      <img src={img} alt="person-img" className="person-img"></img>
+      <div className="person-info">
+        <div className="person-action">
+          <h4>{nome}</h4>
+          <button className="btn" onClick={() => removeItem(id)}>
+            <RiDeleteBackFill className="icon" />
+          </button>
+        </div>
+        <p>{stato}</p>
+      </div>
+    </article>
+  );
 };
 
 export default List;
