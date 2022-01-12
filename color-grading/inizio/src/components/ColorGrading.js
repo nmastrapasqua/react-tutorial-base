54927f80658b0148ae804eaea5da0783d42c49f8
Nunzio Mastrapasqua <nunzio.mastrapasqua@libero.it>
2022-01-12 17:28:14 +0100

    Fine progetto 'Color grading'

--- color-grading/inizio/src/components/ColorGrading.js
+++ color-grading/inizio/src/components/ColorGrading.js
@@ -4,7 +4,87 @@ import SingleColor from "./SingleColor";
 import { v4 as uuidv4 } from "uuid";
 
 const ColorGrading = () => {
-  return <div>Color Grading</div>;
+  const [isError, setIsError] = useState(false);
+  const [selectedColor, setSelectedColor] = useState([]);
+
+  const [colorInput, setColorInput] = useState({
+    color: "",
+    qty: 5,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (colorInput.color && colorInput.qty) {
+      const { color, qty } = colorInput;
+      try {
+        setSelectedColor(
+          new Values(color).all(Math.round(100 / parseInt(qty, 10)) * 2)
+        );
+        setColorInput({
+          color: "",
+          qty: 10,
+        });
+      } catch (error) {
+        setIsError(true);
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    if (isError) {
+      setIsError(false);
+    }
+
+    const { name, value } = e.target;
+    setColorInput({ ...colorInput, [name]: value });
+  };
+
+  // Valore iniziale
+  useEffect(() => {
+    setColorInput({ qty: 10, color: "" });
+    setSelectedColor(new Values("#1194ec").all(Math.round(100 / 10) * 2));
+  }, []);
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <input
+            type="text"
+            name="color"
+            id="color"
+            value={colorInput.color}
+            maxLength={7}
+            onChange={handleChange}
+            className="input"
+          ></input>
+          <input
+            type="number"
+            name="qty"
+            id="qty"
+            value={colorInput.qty}
+            max={100}
+            min={5}
+            step={5}
+            onChange={handleChange}
+            className="input"
+          ></input>
+        </div>
+        <button className="btn btn-selector" type="submit">
+          Create
+        </button>
+      </form>
+      <section className="color-section">
+        {isError ? (
+          <h4 className="section-center">Nessun Colore Trovato</h4>
+        ) : !isError && selectedColor ? (
+          selectedColor.map((el) => <SingleColor key={uuidv4()} {...el} />)
+        ) : (
+          <h4>Loading</h4>
+        )}
+      </section>
+    </>
+  );
 };
 
 export default ColorGrading;
--- color-grading/inizio/src/components/SingleColor.js
+++ color-grading/inizio/src/components/SingleColor.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { rgbToHex } from "../utils/helpers";
 
-const SingleColor = () => {
-  return <div>Single Color</div>;
+const SingleColor = ({ rgb, type, weight }) => {
+  const [message, setMessage] = useState(false);
+
+  const copiaColore = () => {
+    navigator.clipboard
+      .writeText(rgbToHex(...rgb))
+      .then(() => setMessage(true));
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [message]);
+
+  return (
+    <article
+      onClick={copiaColore}
+      className={`single-color ${type}`}
+      style={{ backgroundColor: rgbToHex(...rgb) }}
+    >
+      <h5>{rgbToHex(...rgb)}</h5>
+      {message && <p>Colore Copiato</p>}
+    </article>
+  );
 };
 
 export default SingleColor;
